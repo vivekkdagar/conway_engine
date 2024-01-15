c4c0408a2a1aa595ef67ed209b213adad9d08e37
Vivek Dagar <vivekdagar2023@gmail.com>
2024-01-15 19:56:01 +0530

    Removed classifiers

--- setup.py
+++ setup.py
@@ -13,21 +13,12 @@ setup(
             'game-of-life=conway_engine.simulation:main',
         ],
     },
+    include_package_data=True,
     author='Vivek Dagar',
     author_email='vivekdagar2017@gmail.com',
     description="Conway Engine is a Python package offering a sophisticated implementation"
                 " of Conway's Game of Life, featuring the iconic Gosper Glider Gun for dynamic"
                 " and explosive cellular automaton growth.",
-    url='https://github.com/vivekkdagar/game-of-life',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU General Public License v3.0',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
+    url='https://github.com/vivekkdagar/conway_engine',
     license='GNU General Public License v3.0',
 )
